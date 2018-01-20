b3fb3a871982f92ce53e463e5e398a3119244200
qiujiahong <qiujh@szfangle.com>
2018-01-20 18:13:45 +0800

    pie chart 能够改变数值

--- chart7.pie chart/app.js
+++ chart7.pie chart/app.js
@@ -1,4 +1,5 @@
 var minYear = d3.min(birthData, d => d.year);
+var maxYear = d3.max(birthData, d => d.year);
 var width = 600;
 var height = 600;
 var yearData = birthData.filter(d => d.year === minYear);
@@ -24,20 +25,41 @@ d3.select("svg")
     .attr('transform','translate('+width/2+','+height/2+')')
     .classed("chart",true);
 
-var arcs = d3.pie()
-            .value(d => d.births)
-            (yearData);
 
-var path = d3.arc()
-                .outerRadius(width/2-10)
-                .innerRadius(width/4);
+d3.select('input')
+    .property('min',minYear)
+    .property('max',maxYear)
+    .property('value',minYear)
+    .on("input",function () {
+        makeGraph(+d3.event.target.value)
+    });
 
-d3.select(".chart")
-    .selectAll(".arc")
-    .data(arcs)
-    .enter()
-    .append("path")
-    .classed(".arc",true)
-    .attr('fill',d=> colorScale(d.data.continent))
-    .attr('stroke','black')
-    .attr('d',path);
+makeGraph(minYear);
+
+function  makeGraph(year) {
+    var yearData = birthData.filter(d => d.year === year);
+
+    var arcs = d3.pie()
+        .value(d => d.births)
+        (yearData);
+
+    var path = d3.arc()
+        .outerRadius(width/2-10)
+        .innerRadius(width/4);
+
+    var update = d3.select('.chart')
+        .selectAll('.arc')
+        .data(arcs);
+
+    update.exit().remove();
+
+    update
+        .enter()
+        .append('path')
+        .classed('arc',true)
+        .merge(update)
+        .attr('fill',d=> colorScale(d.data.continent))
+        .attr('stroke','black')
+        .attr('d',path);
+
+}
